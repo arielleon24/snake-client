6716f67419ef205b372af8716f183cdf2f271617
Ariel Leon <arielleon@MacBook-Pro.local>
2020-09-23 15:39:31 -0400

    client created

---
+++ client.js
@@ -0,0 +1,13 @@
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', (data) =>{
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+}

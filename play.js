16ad39360b7fc2ab5634f93f3863c682213e9357
Ariel Leon <arielleon@MacBook-Pro.local>
2020-09-23 20:27:43 -0400

    added setupInput and handeUserInput

--- play.js
+++ play.js
@@ -3,6 +3,24 @@ const { connect } = require('./client');
 /**
  * Establishes connection with the game server
  */
+let connection
+const setupInput = function(conn) {
+  connection = conn; 
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = ("data", key => {
+  if (key === "\u003") {
+    console.log("Byyeeee")
+    process.exit()
+  }
+}); 
+
 
 
 console.log('Connecting ...');
